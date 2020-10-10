5bee3ec66ba6521b0d9988a896bbca06709d4457
ang67 <binikouadiojosias@gmail.com>
2020-10-10 11:36:58 +0200

    Les boutons sur Landing et animation Wolverine

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import { useRef } from "react";
+import React, { useEffect, useState, Fragment  } from 'react';
 
 const Landing = () => {
 
-    return(
+    const [btn, setBtn] = useState(false);
+    const refWolverine = useRef(null);
 
-        <main className="welcomePage">
-            
+    useEffect(()=>{
+        refWolverine.current.classList.add("startingImg");
+        setTimeout(()=> {
+            refWolverine.current.classList.remove("startingImg");
+            setBtn(true);
+        }, 1000)
+    }, [])
+
+    const displayBtn = btn && (
+        <Fragment>
+        <div className="leftBox">
+        <button className="btn-welcome">Inscription</button>
+        </div>
+        <div className="rightBox">
+            <button className="btn-welcome">Inscription</button>
+        </div>
+        </Fragment>
+    )
+     
+
+    return (
+
+        <main ref={refWolverine} className="welcomePage">
+            {displayBtn}
         </main>
 
     )
