a94c56e760f7b9f6362f62f0eaf6db64c289c1de
ang67 <binikouadiojosias@gmail.com>
2020-10-12 01:19:24 +0200

    Instancier la class Firebase via le Contexte

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Firebase, { FirebaseContext} from './components/Firebase';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    <FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>,
   document.getElementById('root')
 );
 
