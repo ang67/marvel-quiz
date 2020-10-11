c01a93abb9a55e0e85b2ca6bc47f248f46d298cd
ang67 <binikouadiojosias@gmail.com>
2020-10-12 00:50:04 +0200

    Les images et le inline CSS dans error Page

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import batman from '../../images/batman.png'
 
+const centerH2 = {
+    textAlign: 'center',
+    marginTop: '50px'
+}
+
+const centerImg = {
+    display: "block",
+    margin: "40px auto"
+}
 const ErrorPage = () => {
     return (
         <div className="quiz-bg">
             <div className="container">
-                ErrorPage
+                <h2 style={centerH2}>Oups, Cette page n'existe pas!</h2>
+                <img src={batman} alt="error page" style={centerImg}/>
             </div>
         </div>
     );
