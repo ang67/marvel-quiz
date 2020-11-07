991649bd78767879248e3bbc43c21d85f0115670
ang67 <binikouadiojosias@gmail.com>
2020-11-08 00:15:08 +0100

    LocalStorage pour stocker la datas de Marvel API

--- src/components/QuizOver/index.js
+++ src/components/QuizOver/index.js
@@ -25,21 +25,51 @@ const QuizOver = React.forwardRef((props, ref) => {
     console.log(asked);
     useEffect(() => {
         setAsked(ref.current)
+
+        if ( localStorage.getItem('marvelStorageDate')) {
+            const date = localStorage.getItem('marvelStorageDate')
+            checkDataAge(date)
+        }
     }, [ref])
 
+    const checkDataAge = date => {
+        var today = Date.now();
+        const timeDifference = today - date;
+        const daysDifference = timeDifference / (1000 * 3600 *24);
+
+        if ( daysDifference >= 15 ) {
+            localStorage.clear();
+            localStorage.setItem('marvelStorageDate', Date.now());
+        }
+    }
     const showModal = id => {
         setOpenModal(true);
 
-        axios
-        .get(`https://gateway.marvel.com/v1/public/characters/${id}?ts=1&apikey=${API_PUBLIC_KEY}&hash=${hash}`)
-        .then( response => {
-            console.log(response);
-            setCharacterInfos(response.data);
+        if ( localStorage.getItem(id)) {
+
+            setCharacterInfos(JSON.parse(localStorage.getItem(id)));
             setLoading(false);
-        })
-        .catch(error => {
-            console.log(error);
-        })
+
+        } else {
+            
+            axios
+            .get(`https://gateway.marvel.com/v1/public/characters/${id}?ts=1&apikey=${API_PUBLIC_KEY}&hash=${hash}`)
+            .then( response => {
+                console.log(response);
+                setCharacterInfos(response.data);
+                setLoading(false);
+
+                localStorage.setItem(id, JSON.stringify(response.data));
+                if ( !localStorage.getItem('marvelStorageDate')) {
+                    localStorage.setItem('marvelStorageDate', Date.now());
+                }
+                
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        }
+        
     }
 
     const hideModal = () => {
