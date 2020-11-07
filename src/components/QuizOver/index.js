446825bf4cb9ee936d17a6f48a89170b9cf0a290
ang67 <binikouadiojosias@gmail.com>
2020-11-07 01:16:57 +0100

    Afficher un Modal (PopUp) dans React

--- src/components/QuizOver/index.js
+++ src/components/QuizOver/index.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import  {GiTrophyCup} from 'react-icons/gi'
 import Loader from '../Loader';
+import Modal from '../Modal';
+
 const QuizOver = React.forwardRef((props, ref) => {
 
     const {
@@ -12,11 +14,20 @@ const QuizOver = React.forwardRef((props, ref) => {
         loadLevelQuestions
     } = props;
 
-    const [asked, setAsked] = useState([])
+    const [asked, setAsked] = useState([]);
+    const [openModal, setOpenModal] = useState(false);
     console.log(asked);
     useEffect(() => {
         setAsked(ref.current)
     }, [ref])
+
+    const showModal = id => {
+        setOpenModal(true);
+    }
+
+    const hideModal = () => {
+        setOpenModal(false);
+    }
 
     const averageGrade = maxQuestions / 2;
 
@@ -75,7 +86,10 @@ const QuizOver = React.forwardRef((props, ref) => {
                     <td>{question.question}</td>
                     <td>{question.answer}</td>
                     <td>
-                        <button className="btnInfo">Infos</button>
+                        <button 
+                            className="btnInfo" 
+                            onClick={() => showModal(question.heroId) }
+                        >Infos</button>
                     </td>
                 </tr>
             )
@@ -115,6 +129,18 @@ const QuizOver = React.forwardRef((props, ref) => {
                     </tbody>
                 </table>
             </div>
+            <Modal showModal={openModal} hideModal={hideModal}>
+                <div className="modalHeader">
+                    <h2>Titre</h2>
+                </div>
+                <div className="modalBody">
+                    <h3>Titre 2</h3>
+                </div>
+
+                <div className="modalFooter">
+                    <button className="modalBtn">Fermer</button>
+                </div>
+            </Modal>
         </Fragment>
     )
 }
