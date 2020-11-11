bb0fbf6013eb951ae64c89da1e20323107fc7231
ang67 <binikouadiojosias@gmail.com>
2020-11-11 16:19:13 +0100

    Les règles de sécurité  Variables d'environnement

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,14 +2,14 @@ import app from 'firebase/app'
 import 'firebase/auth';
 import 'firebase/firestore'
 
-const config = {
-    apiKey: "AIzaSyAvqZ-sjLKO_WWPMgKuF_LPkHCx3ctz8DI",
-    authDomain: "marvel-quiz-93d57.firebaseapp.com",
-    databaseURL: "https://marvel-quiz-93d57.firebaseio.com",
-    projectId: "marvel-quiz-93d57",
-    storageBucket: "marvel-quiz-93d57.appspot.com",
-    messagingSenderId: "924582896425",
-    appId: "1:924582896425:web:d8461dc6e4e57e15ce8d38"
+const config = {    
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
 class Firebase {
